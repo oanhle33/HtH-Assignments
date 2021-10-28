78fd7aa0aa845e548db41713f1b5b5a27945f947
oanhle33 <lanoanh33@gmail.com>
2021-10-27 23:55:20 -0700

    variables_datatypes.py

---
+++ variables_datatypes.py
@@ -0,0 +1,84 @@
+print("Hello World!")
+my_text = "I really enjoy software engineering."
+print(my_text)
+
+print("hack, the, hood")
+
+glass_of_water=6
+print("I drank", glass_of_water, "glasses of water today.")
+
+glass_of_water=3
+glass_of_water=glass_of_water + 1
+print(glass_of_water)
+
+men_stepped_on_tge_moon=9
+print(men_stepped_on_tge_moon)
+
+my_reason_for_coding="solve problems"
+print(my_reason_for_coding)
+
+global_mean_sea_level_2018=21
+global_mean_sea_level_2018=22.46
+print(global_mean_sea_level_2018)
+
+staying_alive="True"
+print(staying_alive)
+
+
+men_stepped_on_the_moon=12
+answer_1= type(men_stepped_on_the_moon)
+print(answer_1)
+
+my_reason_for_coding="intergalactic travel"
+answer_2=type(my_reason_for_coding)
+print(answer_2)
+
+global_mean_sea_level_delta_2018=21.36
+answer_3= type(global_mean_sea_level_delta_2018)
+print(answer_3)
+
+staying_alive=True
+answer_4=type(staying_alive)
+print(answer_4)
+
+my_grade="10"
+answer_5=int("10")
+print(answer_5)
+
+my_temp=97.70
+answer_6=int(97.70)
+print(answer_6)
+
+shoe_price="69.99"
+answer_7=float("69.99")
+print(answer_7)
+
+gross_world_product=84.84
+gwp_str=str(gross_world_product)
+answer_8="In 2018 gross product of the world (GWP) was " + gwp_str + " in trillion US dollars."
+print(answer_8)
+
+gift_list=[]
+answer_9=gift_list
+print(answer_9)
+
+gift_list=[]
+answer_9=type(gift_list)
+print(answer_9)
+
+grocery_items={}
+answer_10=type(grocery_items)
+print(answer_10)
+
+bucket_list=()
+answer_11=type(bucket_list)
+print(answer_11)
+
+gift_list=['shoes', 'hoodies', 'jeans']
+print(gift_list)
+
+grocery_list={1: 'beef', 2: 'shrimps', 3: 'chicken'}
+print(grocery_list)
+
+bucket_list=('code python', 'hang out', 'watch movies')
+print(bucket_list)
